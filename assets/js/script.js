50110947147f477d8e0a4b0ff491b69754a526cd
Kate Elliott <kateelliott145@gmail.com>
2024-11-22 16:56:51 +0000

    add calculate correct answer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,21 @@ function checkAnswer() {
 
 }
 
+/**
+ * gets the operands (the numbers) and the operater (plus, minus, etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alsert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
